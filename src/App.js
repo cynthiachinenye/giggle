6f8618669cb556c92d8f9db6d3c45da7daff7e2b
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-21 14:56:42 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './Pages/Home'
 
 function App() {
   return (
     <div className="App">
+       
+       <Router>
        <Navbar/>
+       <Routes>
+        <Route path='/' exact component={Home}/>
+       </Routes>
+       </Router>
        
 
     </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Logo from '../Asset/pizza-logo-removebg-preview.png'
-
+import {Link} from 'react-router-dom'
+import '../Styles/Navbar.css'
 function Navbar() {
   return (
     <div className="navbar">
       
       <div className='leftside'>
-          <img src = {logo}/>
+          <img src = {Logo}/>
          
       </div>
       
-      <div className='rightside'></div>
+      <div className='rightside'>
+       <Link to='/'> Home</Link>
+       <Link to='menu'>Menu</Link>
+       <Link to='about'> About Us</Link>
+       <Link to='contact'> Contact</Link>
+      </div>
       
     </div>
   )
