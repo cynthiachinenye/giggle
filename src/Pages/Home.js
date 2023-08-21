0e005c4d9b8b1d801e6a586b8f746585bb6d8822
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-21 16:07:05 +0100

    new

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Banner from '../Asset/pexels-2.jpeg'
+import BannerImage from '../Asset/pexels-photo-5.jpeg'
 
 function Home() {
   return (
     <div className='home'>
        
-      <div className='headerContainer' style={backgroundImage: `url( ${Banner})`}>
+      <div className="headerContainer" style={{backgroundImage: `url( ${BannerImage})`}}>
           <h1>Cyndy's Pizzeria</h1>
           <p> PIZZA TO FIT ANY TASTE</p>
       </div>
