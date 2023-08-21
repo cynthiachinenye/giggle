38acf3c097fda3f76cd633ed1d716e1db8bfbc2d
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-21 13:13:56 +0100

    new

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Logo from '../Asset/pizza-logo-removebg-preview.png'
 
 function Navbar() {
   return (
     <div className="navbar">
       
-      <div className='leftside'></div>
-      <img src=""/>
+      <div className='leftside'>
+          <img src = {logo}/>
+         
+      </div>
+      
       <div className='rightside'></div>
       
     </div>
