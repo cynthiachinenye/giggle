c7833915f08453ca2ed4c4911cb917fa1afa59a4
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-22 13:31:42 +0100

    new

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './Pages/Home'
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
        <Routes>
         <Route path='/' exact Component= {Home} />
        </Routes>
+       <Footer/>
        </Router>
        
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Instagram from '@mui/icons-material/Instagram'
+import Twitter from '@mui/icons-material/Twitter'
+import Facebook from '@mui/icons-material/Facebook'
+import Whatsapp from '@mui/icons-material/LinkedIn'
+function Footer() {
+  return (
+    <div className="footer">
+       <div className="socialmedia">
+           <Instagram/>
+           <Twitter/>
+           <Facebook/>
+           <Whatsapp/>
+         
+       </div>
+        <p> &copy; 2023 cyndymeng.com</p>
+    </div>
+  )
+}
+
+export default Footer
